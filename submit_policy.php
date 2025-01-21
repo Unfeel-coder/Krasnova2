2f16dd207726c850ede55df081b7b974eb106823
Unfeel-coder <galex.ind@gmail.com>
2025-01-21 21:35:58 +0700

    Update submit_policy.php

--- submit_policy.php
+++ submit_policy.php
@@ -1,19 +1,1 @@
-<?php
 
-$name = $_POST['name'];
-$passport = $_POST['passport'];
-$address = $_POST['address'];
-$mortgage_amount = $_POST['mortgage_amount'];
-$property_type = $_POST['property_type'];
-$gender = $_POST['gender'];
-
-// Сохранить данные полиса в базе данных
-
-$response = [
-  'success' => true,
-  'error' => null
-];
-
-echo json_encode($response);
-
-?>
